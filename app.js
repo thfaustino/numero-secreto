30827f27cb8c5526120a5f12537a54b351ddc5d9
Thiago Faustino <thfaustino@gmail.com>
2023-11-10 16:02:46 -0300

    Mudanca teste

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('reiniciar').removeAttribute(nhjnn'disabled');nnn
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
